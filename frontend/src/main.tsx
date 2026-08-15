f4461244296d900b0b477982a98a0c9ab0d77b90
baileykv75-netizen <baileykv75@gmail.com>
2026-08-15 17:31:49 +0800

    stage7 mount legal retrieval panel

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -2,11 +2,13 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App'
 import LegalKnowledgePanel from './LegalKnowledgePanel'
+import LegalRetrievalPanel from './LegalRetrievalPanel'
 import './styles.css'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
     <LegalKnowledgePanel />
+    <LegalRetrievalPanel />
   </StrictMode>,
 )
